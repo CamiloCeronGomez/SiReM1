58eb6fa4f840682db5d29556f2dc82c640616963
CamiloCeronGomez <camiloceron1990@gmail.com>
2017-09-11 16:32:56 -0500

    bottomNavigation y funcionalidad menu info

--- app/src/main/java/com/example/camilo/sirem/PerfilActivity.java
+++ app/src/main/java/com/example/camilo/sirem/PerfilActivity.java
@@ -1,33 +1,21 @@
 package com.example.camilo.sirem;
 
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.camilo.sirem.databinding.ActivityMainBinding;
-import com.example.camilo.sirem.fragments.MainFragment;
+public class PerfilActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
-        putFragment(R.id.container, MainFragment.instance());
-
-        binding.setHandlerMain(this);
-
+        setContentView(R.layout.activity_perfil);
         //region Option Bottom Navigation Menu
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.btnBottom);
         bottomNavigationView.setOnNavigationItemSelectedListener(
@@ -37,18 +25,18 @@ public class MainActivity extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.btnTratamiento:
 
-                                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                                Intent intent = new Intent(PerfilActivity.this, MainActivity.class);
                                 startActivity(intent);
 
                                 break;
 
                             case R.id.btnProgreso:
-                                Intent intent2 = new Intent(MainActivity.this, ProgresoActivity.class);
+                                Intent intent2 = new Intent(PerfilActivity.this, ProgresoActivity.class);
                                 startActivity(intent2);
                                 break;
 
                             case R.id.btnPerfil:
-                                Intent intent3 = new Intent(MainActivity.this, PerfilActivity.class);
+                                Intent intent3 = new Intent(PerfilActivity.this, PerfilActivity.class);
                                 startActivity(intent3);
                                 break;
 
@@ -58,15 +46,8 @@ public class MainActivity extends AppCompatActivity {
                 });
         //endregion
 
+
     }
-
-    public void putFragment(int container, Fragment fragment) {
-
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(container, fragment);
-        ft.commit();
-    }
-
     //region Option Info Menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -79,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_info:
 
-                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                Intent intent = new Intent(PerfilActivity.this, InfoActivity.class);
                 startActivity(intent);
 
                 break;
@@ -88,3 +69,5 @@ public class MainActivity extends AppCompatActivity {
     }
     //endregion
 }
+
+
--- app/src/main/java/com/example/camilo/sirem/ProgresoActivity.java
+++ app/src/main/java/com/example/camilo/sirem/ProgresoActivity.java
@@ -1,32 +1,20 @@
 package com.example.camilo.sirem;
 
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.camilo.sirem.databinding.ActivityMainBinding;
-import com.example.camilo.sirem.fragments.MainFragment;
-
-public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
+public class ProgresoActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
-        putFragment(R.id.container, MainFragment.instance());
-
-        binding.setHandlerMain(this);
+        setContentView(R.layout.activity_progreso);
 
         //region Option Bottom Navigation Menu
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.btnBottom);
@@ -37,18 +25,18 @@ public class MainActivity extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.btnTratamiento:
 
-                                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                                Intent intent = new Intent(ProgresoActivity.this, MainActivity.class);
                                 startActivity(intent);
 
                                 break;
 
                             case R.id.btnProgreso:
-                                Intent intent2 = new Intent(MainActivity.this, ProgresoActivity.class);
+                                Intent intent2 = new Intent(ProgresoActivity.this, ProgresoActivity.class);
                                 startActivity(intent2);
                                 break;
 
                             case R.id.btnPerfil:
-                                Intent intent3 = new Intent(MainActivity.this, PerfilActivity.class);
+                                Intent intent3 = new Intent(ProgresoActivity.this, PerfilActivity.class);
                                 startActivity(intent3);
                                 break;
 
@@ -59,14 +47,6 @@ public class MainActivity extends AppCompatActivity {
         //endregion
 
     }
-
-    public void putFragment(int container, Fragment fragment) {
-
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(container, fragment);
-        ft.commit();
-    }
-
     //region Option Info Menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -79,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_info:
 
-                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                Intent intent = new Intent(ProgresoActivity.this, InfoActivity.class);
                 startActivity(intent);
 
                 break;
@@ -88,3 +68,4 @@ public class MainActivity extends AppCompatActivity {
     }
     //endregion
 }
+
