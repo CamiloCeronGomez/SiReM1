e714f92c938080737d2a52d42c124d1734f4e820
CamiloCeronGomez <camiloceron1990@gmail.com>
2018-01-30 15:24:36 -0500

    integracion de prueba con sensor acelerometro

--- app/src/main/java/com/example/camilo/sirem/EspereActivity.java
+++ app/src/main/java/com/example/camilo/sirem/EspereActivity.java
@@ -1,35 +1,56 @@
 package com.example.camilo.sirem;
 
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.camilo.sirem.databinding.ActivityEspereBinding;
 
-public class EspereActivity extends AppCompatActivity {
+public class EspereActivity extends AppCompatActivity implements SensorEventListener{
 
     ActivityEspereBinding binding;
 
     private LinearLayout layoutEspere;
     private LinearLayout layoutPrimerInicia;
     private LinearLayout layoutPrimerAngulo;
+    private TextView txtPrimerAngulo;
     private LinearLayout layoutSegundoInicia;
     private LinearLayout layoutSegundoAngulo;
+    private TextView txtSegundoAngulo;
+
+    private float Ac = 0;
+
+    private SensorManager sensorManager;
+    private Sensor acelerometro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this, R.layout.activity_espere);
         binding.setHandler(this);
+
         layoutEspere = (LinearLayout)findViewById(R.id.layoutEspere);
         layoutPrimerInicia = (LinearLayout)findViewById(R.id.layoutPrimerInicia);
         layoutPrimerAngulo = (LinearLayout)findViewById(R.id.layoutPrimerAngulo);
+        txtPrimerAngulo = (TextView) findViewById(R.id.txtPrimerAngulo);
         layoutSegundoInicia = (LinearLayout)findViewById(R.id.layoutSegundoInicia);
         layoutSegundoAngulo = (LinearLayout)findViewById(R.id.layoutSegundoAngulo);
+        txtSegundoAngulo = (TextView)findViewById(R.id.txtSegundoAngulo);
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         layoutEspere.setVisibility(View.VISIBLE);
         layoutPrimerInicia.setVisibility(View.INVISIBLE);
@@ -37,7 +58,27 @@ public class EspereActivity extends AppCompatActivity {
         layoutSegundoInicia.setVisibility(View.INVISIBLE);
         layoutSegundoAngulo.setVisibility(View.INVISIBLE);
 
+        if (acelerometro == null)
+            finish();
+
         primerinicia();
+    }
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    protected  void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        Ac = event.values[0];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     private void primerinicia() {
@@ -68,6 +109,8 @@ public class EspereActivity extends AppCompatActivity {
                 layoutPrimerAngulo.setVisibility(View.VISIBLE);
                 layoutSegundoInicia.setVisibility(View.INVISIBLE);
                 layoutSegundoAngulo.setVisibility(View.INVISIBLE);
+
+                txtPrimerAngulo.setText(""+ Ac);
 
                 segundoinicia();
             }
@@ -101,6 +144,8 @@ public class EspereActivity extends AppCompatActivity {
                 layoutPrimerAngulo.setVisibility(View.INVISIBLE);
                 layoutSegundoInicia.setVisibility(View.INVISIBLE);
                 layoutSegundoAngulo.setVisibility(View.VISIBLE);
+
+                txtSegundoAngulo.setText(""+ Ac);
 
 
             }
